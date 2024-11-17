c595ace783e364c20a1df47f3f0e03ebc873775d
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-17 19:57:38 +0600

    some improvements

--- streams.js
+++ streams.js
@@ -3,58 +3,102 @@
 // // let b = a* 1000
 // // console.timeEnd('writeMany'); // performance check
 
-const fs = require('fs/promises'); // Importing the promises API from the 'fs' module
+// const fs = require('fs/promises'); // Importing the promises API from the 'fs' module
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+// -----------------------------------------------------
 
 // IIFE (Immediately Invoked Function Expression) to perform asynchronous file operations
+// (async () => {
+//     // it takes 15 seconds and 83% of cpu single core and 
+//     // Memory usge : around 3gbs
+
+//     // Log initial memory usage
+//    console.log("Initial Memory Usage:", process.memoryUsage());
+
+
+
+//     console.time('writeMany'); // Start measuring the time taken for writing to the file
+
+//     const fileHandle = await fs.open("./texts/new.txt", "w");
+
+//     for (let i = 0; i < 1000000; i++) {
+//         fileHandle.write(` ${i} `); 
+
+//         // Log memory usage periodically (e.g., every 100,000 iterations)
+//         // if (i % 100000 === 0) {
+//             // console.log(`Memory Usage at iteration ${i}:`, process.memoryUsage());
+//         // }
+//     }
+
+//     await fileHandle.close(); // Always close the file handle after writing to avoid resource leaks
+//     console.timeEnd('writeMany'); 
+
+
+//     console.log("Final Memory Usage:", process.memoryUsage());
+// })();
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+// -----------------------------------------------------
+
+
+
+// const fs = require('node:fs'); // Importing the promises API from the 'fs' module
+
+
+
+// (async () => {
+
+//     console.time('writeMany'); // Start measuring the time taken for writing to the file
+//     fs.open("./texts/new.txt", "w", (err, data) => {
+//         for (let i = 0; i < 1000000; i++) {
+//             const buff = Buffer.from(`${i}`, 'utf-8')
+//             fs.writeSync(data, buff);
+//         }
+//     });
+
+
+//     console.timeEnd('writeMany');
+
+//     // console.log("Final Memory Usage:", process.memoryUsage());
+// })();
+
+
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+// -----------------------------------------------------
+
+
+// another version with streams
+// fastest one with 570 ms times to complete writing
+
+const fs = require('node:fs/promises'); // Importing the promises API from the 'fs' module
+
+
+
 (async () => {
-    // it takes 15 seconds and 83% of cpu single core and 
-    // Memory usge : around 3gbs
-
-    // Log initial memory usage
-   console.log("Initial Memory Usage:", process.memoryUsage());
-
-
 
     console.time('writeMany'); // Start measuring the time taken for writing to the file
 
-    const fileHandle = await fs.open("./texts/new.txt", "w");
-    
-    for (let i = 0; i < 1000000; i++) {
-        fileHandle.write(` ${i} `); 
+    const fileHandle = await fs.open("./texts/new.txt", "w",)
 
-        // Log memory usage periodically (e.g., every 100,000 iterations)
-        // if (i % 100000 === 0) {
-            // console.log(`Memory Usage at iteration ${i}:`, process.memoryUsage());
-        // }
+    const stream = fileHandle.createWriteStream()
+
+    for (let i = 0; i < 1000000; i++) {
+        const buff = Buffer.from(`${i}`, 'utf-8')
+        stream.write(buff);
     }
 
-    await fileHandle.close(); // Always close the file handle after writing to avoid resource leaks
-    console.timeEnd('writeMany'); 
+    console.timeEnd('writeMany');
 
-
-    console.log("Final Memory Usage:", process.memoryUsage());
+    // console.log("Final Memory Usage:", process.memoryUsage());
 })();
 
 
+// -----------------------------------------------------
+// -----------------------------------------------------
+// -----------------------------------------------------
 
-
-// -------------------------------------------------------------
-// same Task But with improved performance and memory usage
-// --------------------------------------------------------------
-
-// takes only 7s to complete the writing 
-
-const fs = require('fs');
-
-const writeStream = fs.createWriteStream('./texts/new.txt');
-
-console.time('writeMany');
-
-for (let i = 0; i < 1000000; i++) {
-    writeStream.write(` ${i} `);
-}
-
-writeStream.end(() => {
-    console.timeEnd('writeMany');
-    console.log("Final Memory Usage:", process.memoryUsage());
-});
